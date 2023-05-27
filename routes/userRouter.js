48370c5bd10bac21f2fdc840f1cf7c14ba1f8d6a
hasanmiadev <hasanmiadev@gmail.com>
2023-05-27 08:56:27 +0600

    check git

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,5 +2,5 @@ const { getAllUser } = require('../controllers/userController');
 const { authMiddleware } = require('../middlewares/auth');
 
 const userRoute = require('express').Router();
-userRoute.get('/getAllUser',authMiddleware, getAllUser )
+userRoute.get('/getAllUser',authMiddlewares, getAllUser )
 module.exports = userRoute;
